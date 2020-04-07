a55184da6256bd2ef6a3fa1d706a8993bd07a826
michaelzhao0329 <60829192+michaelzhao0329@users.noreply.github.com>
2020-04-07 09:34:38 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -112,6 +112,4 @@ app.get("/posts/:postId", function(req, res){
 //  res.redirect("/");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
-});
+app.listen(process.env.PORT || 3000, () => console.log(`Example app listening on port "3000"`))
